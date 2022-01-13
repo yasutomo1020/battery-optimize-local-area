794ddd310befe69bab01775c038224d8a0412a97
yasutomo1020 <yasutomo1020@gmail.com>
2022-01-13 12:03:48 +0900

    y軸桁数，フォント変更

--- figure_out.m
+++ figure_out.m
@@ -1,4 +1,4 @@
-function figure_out(type,figure_name,data,xll,yll,xlabel_name,ylabel_name,pos,les,color,save)
+function figure_out(type,figure_name,data,xll,yll,xlabel_name,ylabel_name,pos,les,color,yf,save)
 %図を出力し保存する関数
 
 if type == "bar"
@@ -15,14 +15,11 @@ if type == "bar"
     ylabel(ylabel_name)
     pbaspect([2 1 1])
     h=gca;
-    set(h,'fontsize',18);
-    set(gca,'GridLineStyle',':',... % グリッドの線種を点線に設定
-        'GridColor',[10 10 10]/255,... % グリッド線の色を黒に設定
-        'GridAlpha',1,'LineWidth',1) % 透過性を.3，グリッド線の太さを.5に設定
+  set(h,'fontsize',18,'GridLineStyle',':','GridColor',[10 10 10]/255,'GridAlpha',1,'LineWidth',1,'FontName','ＭＳ 明朝')
     if isempty(les)==0
         %legend(les,'Location','southwest')
         legend(les)
-        legend('boxoff')
+  %      legend('boxoff')
     end
 elseif type == "plot"
     a=figure('Name',num2str(figure_name),'ToolBar','none','NumberTitle','on','Units','normalized','OuterPosition',pos);
@@ -36,16 +33,15 @@ elseif type == "plot"
     xticklabels({'6:00','9:00','12:00','15:00','18:00','21:00','0:00','3:00','6:00',})
     xlabel(xlabel_name)
     ylabel(ylabel_name)
+    ytickformat(yf)
     pbaspect([2 1 1])
     h=gca;
-    set(h,'fontsize',18);
-    set(gca,'GridLineStyle',':',... % グリッドの線種を点線に設定
-        'GridColor',[10 10 10]/255,... % グリッド線の色を黒に設定
-        'GridAlpha',1,'LineWidth',1) % 透過性を.3，グリッド線の太さを.5に設定
+    set(h,'fontsize',18,'GridLineStyle',':','GridColor',[10 10 10]/255,'GridAlpha',1,'LineWidth',1,'FontName','ＭＳ 明朝')
     if isempty(les)==0
+         led=legend(les);
         % legend(les,'Location','southwest')
-        legend(les)
-        legend('boxoff')
+        led.Color='white';
+   %    legend('boxoff')
     end
     pbaspect([1.5 1 1]);
 elseif type == "plot_big"
@@ -62,17 +58,15 @@ elseif type == "plot_big"
     ylabel(ylabel_name)
     pbaspect([2 1 1])
     h=gca;
-    %set(h,'fontsize',24);
-    set(gca,'GridLineStyle',':',... % グリッドの線種を点線に設定
-        'GridColor',[10 10 10]/255,... % グリッド線の色を黒に設定
-        'GridAlpha',1,'LineWidth',1) % 透過性を.3，グリッド線の太さを.5に設定
+  set(h,'GridLineStyle',':','GridColor',[10 10 10]/255,'GridAlpha',1,'LineWidth',1,'FontName','ＭＳ 明朝')
     if isempty(les)==0
         % legend(les,'Location','southwest')
         led=legend(les);
         led.NumColumns = 2;
         led.Location='northeast';
         led.FontSize=20;
-        legend('boxoff')
+        led.Color='w';
+     %   legend('boxoff')
     end
     set(h,'fontsize',20);
     pbaspect([1.5 1 1]);
@@ -97,10 +91,10 @@ grid on
 %  print(ylabel_name, '-dpng', '-r450')%png image output
 if save==1
     a.OuterPosition=pos*2;
-    img_name=string(figure_name)+'.emf';
+    img_name='figure/'+string(figure_name)+'.emf';
     exportgraphics(a,img_name,'Resolution',500,'ContentType','vector')
     a.OuterPosition=pos;
     disp(string(img_name)+'を保存しました。')
 end
-set(h,'fontsize',12);
+set(h,'fontsize',10);
 end
